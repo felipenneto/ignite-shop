2a85c6b90dc6e82b57e4bd1edb290aab088ff355
Felipe Neto <106479636+felipenneto@users.noreply.github.com>
2024-01-12 22:01:30 -0300

    Add files via upload

---
+++ ignite-shop-main/src/pages/product/[id].tsx
@@ -0,0 +1,114 @@
+import axios from "axios";
+import Stripe from "stripe";
+
+import Image from "next/image";
+
+import { GetStaticPaths, GetStaticProps } from "next";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+import { stripe } from "../../lib/stripe";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
+import Head from "next/head";
+
+interface ProductProps {
+  product: {
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: string,
+    description: string,
+    defaultPriceId: string,
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl
+
+    } catch (err) {
+      // Ideal => conectar com uma ferramenta de observabilidade (Datadog / sentry)
+
+      setIsCreatingCheckoutSession(false)
+      alert('Falha ao redirecionar ao checkout');
+    }
+  }
+
+  const { isFallback } = useRouter()
+
+  if (isFallback) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        </ImageContainer>
+        <ProductDetails>
+
+          <h1>{product.name}</h1>
+
+          <span>{product.price}</span>
+
+          <p>{product.description}</p>
+
+          <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>Comprar Agora</button>
+
+        </ProductDetails>
+      </ProductContainer>
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { id: 'prod_OD2VIC4ey2U05F' } }
+    ],
+    fallback: true,
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount / 100),
+        description: product.description,
+        defaultPriceId: price.id,
+      }
+    },
+    revalidate: 60 * 60 * 1 // 1 hour
+  }
+}
